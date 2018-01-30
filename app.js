e8b52f1ee7fcd5539c6826b2d78d7a10d093fd8f
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2018-01-30 12:35:51 +0000

    basic working view

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(routes);
+
+app.listen(app.get('port'), function() {
+  console.log('Server started on ' + app.get('port'));
+});
