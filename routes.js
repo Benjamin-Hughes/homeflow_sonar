e235de670331e967485c83660d8f76aeedd25ea4
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2018-01-30 15:50:10 +0000

    add post route for form and formatting for results on page

--- routes.js
+++ routes.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 router.get("/", function(req, res) {
   res.render('index');
 });
 
+router.post("/", function(req, res) {
+  
+  request(process.env.REQUEST, function(error, response, body) {
+  var filteredResults = [];
+  var results = JSON.parse(body);
+  var propertyList = Array.from(results.result.properties.elements);
+
+    propertyList.forEach(function(property) {
+
+      if (property.bedrooms >= req.body.beds && property.price_value <= req.body.pricemax) {
+        filteredResults.push(property);
+      }
+    })
+
+    res.render('index', {results: filteredResults});
+  })
+})
 module.exports = router;
